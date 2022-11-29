448ce09a9f117f479471c9549af91ce92976fe8f
OmarMhmd <algburi89@gmail.com>
2022-11-28 22:17:18 -0600

    ghgh

---
+++ burger/index.php
@@ -0,0 +1,72 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	
+	<link rel="stylesheet" type="text/css" href="css/normalize1.css">
+	<link rel="stylesheet" type="text/css" href="css/style4.css">
+</head>
+<title>my page</title>
+<body>
+		
+		<!--header starte here-->
+		<header>	
+				
+			<ul >
+				<div class="container">
+				<li><a href="#.php">click me the title</a></li>
+				<li><a href="#.php">about</a></li>
+				<li><a href="#.php">menu</a></li>
+				<li><a href="#.php">contact</a></li>
+			</div>
+			</ul>
+			<div class="text-me">
+				<h1>Le Catering</h1>
+			</div>
+		</header>
+			<!--header ends here-->
+
+			<!--slider starte here-->
+			<div class="slider">
+			<div class="container">
+				<div class="left"></div>
+				<article>
+				<h1>About Catering</h1> 
+				<h2>Tradition since 1889</h2>
+					<p>The Catering was founded in blabla by Mr. Smith in lorem ipsum dolor sit amet, consectetur adipiscing elit consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute iruredolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.We only use seasonal ingredients.</p>
+
+					<p>Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum consectetur adipiscing elit, sed do eiusmod temporincididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+
+				</article>
+			</div>
+			</div>
+			<!--slider ends here-->
+
+			<!--catering starts here-->
+			<div class="catering">
+				third
+			</div>
+			<!--catering starts here-->
+
+			<!--menu starts here-->
+			<div class="menu">
+				four
+				
+
+			</div>
+			<!--menu ends here-->
+
+			<!--contact starts here-->
+			<div class="contact">
+				five
+			</div>
+			<!--contact ends here-->
+
+			<!--contact starts here-->
+			<footer>
+				six
+			</footer>
+			<!--contact ends here-->
+</body>
+
+</html>
